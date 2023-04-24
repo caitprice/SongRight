8aebada18a496a42bc34214639dff8e8c2730585
caitprice <caitlinp19@icloud.com>
2023-04-24 12:59:58 -0400

    Initial Commit

---
+++ SongRight/SongRightApp.swift
@@ -0,0 +1,17 @@
+//
+//  SongRightApp.swift
+//  SongRight
+//
+//  Created by Caitlin Price on 4/24/23.
+//
+
+import SwiftUI
+
+@main
+struct SongRightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
